8ee41309af9683f722ae7ebff20f3dc58ec364f5
Karina <karinaslusar246@gmail.com>
2024-02-20 20:22:28 +0200

    lab1-1

--- Lab1/MyForm.cpp
+++ Lab1/MyForm.cpp
@@ -1,2 +1,9 @@
 #include "MyForm.h"
-
+#include <Windows.h>
+using namespace Lab1; // Project name
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cmath>
+
 
 namespace Lab1 {
 
@@ -34,6 +36,16 @@ namespace Lab1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 
 	private:
 		/// <summary>
@@ -48,18 +60,171 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(96, 157);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(51, 31);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"x =";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(153, 154);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(299, 38);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(44, 9);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(493, 39);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Обчислення значення функції";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(75, 314);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(127, 39);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Обчислити";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(245, 314);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(127, 39);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Очистити";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(12, 442);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(138, 17);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"© 2024, Біла Каріна";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(96, 221);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(51, 31);
+			this->label4->TabIndex = 6;
+			this->label4->Text = L"y =";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(153, 221);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(299, 38);
+			this->textBox2->TabIndex = 7;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(138, 51);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(314, 83);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->pictureBox1->TabIndex = 8;
+			this->pictureBox1->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1250, 516);
+			this->ClientSize = System::Drawing::Size(630, 468);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Function";
+			this->TopMost = true;
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
-#pragma endregion
-	};
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	//check if the input is correct and is double
+	if (textBox1->Text == "") {
+		MessageBox::Show("Введіть значення x", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		textBox1->Focus();
+		return;
+	}
+	double doubleValue;
+	if (!System::Double::TryParse(textBox1->Text, doubleValue)) {
+		MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		textBox1->Focus();
+		return;
+	}
+
+
+	double x = Convert::ToDouble(textBox1->Text);
+	double y = 0;
+		//y=((ln x^2 + cos x^2) / (cos2x +x^2)) + pow(x, 1/3)/x
+	y = ((log(pow(x, 2)) + cos(pow(x, 2))) / (cos(2 * x) + pow(x, 2))) + pow(x, 1.0 / 3) / x;
+	textBox2->Text = Convert::ToString(y);
+
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox1->Clear();
+	textBox2->Clear();
+	textBox1->Focus();
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	//center the form
+	this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
+		(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
+}
+};
 }
