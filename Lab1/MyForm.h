8cdc0c5bbed206bca117352f7443ad52287f0b38
karina <karinaslusar246@gmail.com>
2024-03-24 14:02:37 +0200

    Lab 3-1

--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -36,22 +36,24 @@ namespace Lab1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+
 	protected:
-	private: System::Windows::Forms::TextBox^ textBox1;
+
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -60,55 +62,30 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(96, 157);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(51, 31);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"x =";
-			// 
-			// textBox1
-			// 
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(153, 154);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(299, 38);
-			this->textBox1->TabIndex = 1;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(44, 9);
+			this->label2->Location = System::Drawing::Point(15, 9);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(493, 39);
+			this->label2->Size = System::Drawing::Size(468, 25);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"Обчислення значення функції";
+			this->label2->Text = L"Пошук максимального та мінімального числа";
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(75, 314);
+			this->button1->Location = System::Drawing::Point(75, 245);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(127, 39);
 			this->button1->TabIndex = 3;
@@ -120,7 +97,7 @@ namespace Lab1 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(245, 314);
+			this->button2->Location = System::Drawing::Point(303, 245);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(127, 39);
 			this->button2->TabIndex = 4;
@@ -133,7 +110,7 @@ namespace Lab1 {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(12, 442);
+			this->label3->Location = System::Drawing::Point(9, 317);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(138, 17);
 			this->label3->TabIndex = 5;
@@ -142,89 +119,73 @@ namespace Lab1 {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(96, 221);
+			this->label4->Location = System::Drawing::Point(71, 52);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(51, 31);
+			this->label4->Size = System::Drawing::Size(344, 21);
 			this->label4->TabIndex = 6;
-			this->label4->Text = L"y =";
+			this->label4->Text = L"Введіть дійсних чисел (розділяючи Enter)";
 			// 
-			// textBox2
+			// richTextBox1
 			// 
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(153, 221);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->ReadOnly = true;
-			this->textBox2->Size = System::Drawing::Size(299, 38);
-			this->textBox2->TabIndex = 7;
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(138, 51);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(314, 83);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
-			this->pictureBox1->TabIndex = 8;
-			this->pictureBox1->TabStop = false;
+			this->richTextBox1->Location = System::Drawing::Point(12, 108);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(470, 96);
+			this->richTextBox1->TabIndex = 9;
+			this->richTextBox1->Text = L"";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(630, 468);
-			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->textBox2);
+			this->ClientSize = System::Drawing::Size(494, 341);
+			this->Controls->Add(this->richTextBox1);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
 			this->Text = L"Function";
 			this->TopMost = true;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	//check if the input is correct and is double
-	if (textBox1->Text == "") {
-		MessageBox::Show("Введіть значення x", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		//перетворення тексту в масив чисел
+		String^ str = richTextBox1->Text;
+		array<String^>^ arr = str->Split('\n');
+
+		//пошук максимального та мінімального числа
+		double max = Convert::ToDouble(arr[0]);
+		double min = Convert::ToDouble(arr[0]);
+		for (int i = 1; i < arr->Length; i++) {
+			double num = Convert::ToDouble(arr[i]);
+			if (num > max) max = num;
+			if (num < min) min = num;
+		}
+
+		//підрахунок різниці між ними
+		double diff = max - min;
+
+		//виведення результату
+		richTextBox1->Clear();
+		richTextBox1->AppendText("Максимальне число: " + max
+			+ "\nМінімальне число: "
+			+ min + "\nРізниця: " + diff);
 	}
-	double doubleValue;
-	if (!System::Double::TryParse(textBox1->Text, doubleValue)) {
-		MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		richTextBox1->Clear();
+		richTextBox1->Focus();
 	}
-
-
-	double x = Convert::ToDouble(textBox1->Text);
-	double y = 0;
-		//y=((ln x^2 + cos x^2) / (cos2x +x^2)) + pow(x, 1/3)/x
-	y = ((log(pow(x, 2)) + cos(pow(x, 2))) / (cos(2 * x) + pow(x, 2))) + pow(x, 1.0 / 3) / x;
-	textBox2->Text = Convert::ToString(y);
-
-}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	textBox1->Clear();
-	textBox2->Clear();
-	textBox1->Focus();
-}
-private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	//center the form
-	this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
-		(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
-}
-};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		//center the form
+		this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
+			(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
+	}
+	};
 }
