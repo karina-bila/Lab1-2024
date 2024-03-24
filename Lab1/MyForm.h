ebc24e84297bbe4b8659f3b84eb59abd88db8cdb
karina <karinaslusar246@gmail.com>
2024-03-24 14:36:53 +0200

    Lab 3-2

--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <cmath>
+#include <math.h>
 
 
 namespace Lab1 {
@@ -43,10 +43,18 @@ namespace Lab1 {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label3;
+
+
+
+
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label4;
-
-
-	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Label^ label6;
 
 
 	private:
@@ -66,8 +74,14 @@ namespace Lab1 {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label2
@@ -75,17 +89,17 @@ namespace Lab1 {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(15, 9);
+			this->label2->Location = System::Drawing::Point(120, 9);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(468, 25);
+			this->label2->Size = System::Drawing::Size(232, 25);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"Пошук максимального та мінімального числа";
+			this->label2->Text = L"Табулювання функцій";
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(75, 245);
+			this->button1->Location = System::Drawing::Point(33, 440);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(127, 39);
 			this->button1->TabIndex = 3;
@@ -97,7 +111,7 @@ namespace Lab1 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(303, 245);
+			this->button2->Location = System::Drawing::Point(270, 440);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(127, 39);
 			this->button2->TabIndex = 4;
@@ -110,45 +124,111 @@ namespace Lab1 {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(9, 317);
+			this->label3->Location = System::Drawing::Point(12, 503);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(138, 17);
 			this->label3->TabIndex = 5;
 			this->label3->Text = L"© 2024, Біла Каріна";
 			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Corbel", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(60, 49);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 27);
+			this->textBox1->TabIndex = 10;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Corbel", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(60, 82);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 27);
+			this->textBox2->TabIndex = 11;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Corbel", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox3->Location = System::Drawing::Point(60, 115);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 27);
+			this->textBox3->TabIndex = 12;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(22, 57);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(32, 19);
+			this->label1->TabIndex = 13;
+			this->label1->Text = L"a =";
+			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.25F, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(71, 52);
+			this->label4->Location = System::Drawing::Point(22, 90);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(344, 21);
-			this->label4->TabIndex = 6;
-			this->label4->Text = L"Введіть дійсних чисел (розділяючи Enter)";
+			this->label4->Size = System::Drawing::Size(32, 19);
+			this->label4->TabIndex = 14;
+			this->label4->Text = L"b =";
 			// 
-			// richTextBox1
+			// label5
 			// 
-			this->richTextBox1->Location = System::Drawing::Point(12, 108);
-			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->Size = System::Drawing::Size(470, 96);
-			this->richTextBox1->TabIndex = 9;
-			this->richTextBox1->Text = L"";
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(22, 123);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(32, 19);
+			this->label5->TabIndex = 15;
+			this->label5->Text = L"h =";
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(26, 179);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(371, 251);
+			this->listBox1->TabIndex = 16;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(200, 51);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(214, 81);
+			this->label6->TabIndex = 17;
+			this->label6->Text = L"y(x) = arctg(x + 3,1)\r\n\r\nz(x) = e^x";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(494, 341);
-			this->Controls->Add(this->richTextBox1);
+			this->ClientSize = System::Drawing::Size(448, 529);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
-			this->Text = L"Function";
+			this->Text = L"Табулювання функцій";
 			this->TopMost = true;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
@@ -156,36 +236,56 @@ namespace Lab1 {
 
 		}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		//перетворення тексту в масив чисел
-		String^ str = richTextBox1->Text;
-		array<String^>^ arr = str->Split('\n');
+		//табулювання функції на відрізку [a, b] з кроком h
 
-		//пошук максимального та мінімального числа
-		double max = Convert::ToDouble(arr[0]);
-		double min = Convert::ToDouble(arr[0]);
-		for (int i = 1; i < arr->Length; i++) {
-			double num = Convert::ToDouble(arr[i]);
-			if (num > max) max = num;
-			if (num < min) min = num;
+		//перевірка на коректність введених даних
+		double a, b, h;
+		try {
+			a = Convert::ToDouble(textBox1->Text);
+			b = Convert::ToDouble(textBox2->Text);
+			h = Convert::ToDouble(textBox3->Text);
+		}
+		catch (FormatException^) {
+			MessageBox::Show("Некоректні дані", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			return;
 		}
 
-		//підрахунок різниці між ними
-		double diff = max - min;
+		if (h <= 0) {
+			MessageBox::Show("Крок має бути більше нуля", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			return;
+		}
 
-		//виведення результату
-		richTextBox1->Clear();
-		richTextBox1->AppendText("Максимальне число: " + max
-			+ "\nМінімальне число: "
-			+ min + "\nРізниця: " + diff);
+		//табулювання функції
+		//функція1: arctg(x+3,1), функція2: e^x
+		listBox1->Items->Clear();
+		double x = a;
+		do {
+			double y = atan(x + 3.1);
+			double z = exp(x);
+			listBox1->Items->Add(String::Format("x = {0:F2},		y = {1:F4},	z = {2:F4}", x, y, z));
+			x += h;
+		} while (x <= b + h);
+
+
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		richTextBox1->Clear();
-		richTextBox1->Focus();
+		//очищення полів
+		textBox1->Clear();
+		textBox2->Clear();
+		textBox3->Clear();
+		listBox1->Items->Clear();
+
+		textBox1->Focus();
 	}
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-		//center the form
+		//центрування форми
 		this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
 			(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
+
+		//встановлення значень за замовчуванням
+		textBox1->Text = "-6";
+		textBox2->Text = "1";
+		textBox3->Text = "0,2";
 	}
 	};
 }
