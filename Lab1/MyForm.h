3ce185b1ecaa33fb92638dba40cc31d57da10cd3
karina <karinaslusar246@gmail.com>
2024-04-09 11:38:08 +0300

    lab 7 -1

--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -38,15 +38,16 @@ namespace Lab1 {
 		}
 
 	protected:
-	private: System::Windows::Forms::TextBox^ textBox1;
+
 
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
+
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
 
 
 
@@ -64,34 +65,24 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
-			// 
-			// textBox1
-			// 
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(12, 116);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(479, 38);
-			this->textBox1->TabIndex = 1;
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(12, 467);
+			this->button1->Location = System::Drawing::Point(12, 318);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(127, 39);
 			this->button1->TabIndex = 3;
-			this->button1->Text = L"Обчислити";
+			this->button1->Text = L"Знайти";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
@@ -99,7 +90,7 @@ namespace Lab1 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(364, 467);
+			this->button2->Location = System::Drawing::Point(364, 318);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(127, 39);
 			this->button2->TabIndex = 4;
@@ -112,7 +103,7 @@ namespace Lab1 {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(12, 529);
+			this->label3->Location = System::Drawing::Point(9, 379);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(138, 17);
 			this->label3->TabIndex = 5;
@@ -123,29 +114,18 @@ namespace Lab1 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(12, 73);
+			this->label1->Location = System::Drawing::Point(9, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(338, 31);
+			this->label1->Size = System::Drawing::Size(233, 31);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Введіть масив через пробіл";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(12, 9);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(522, 39);
-			this->label2->TabIndex = 2;
-			this->label2->Text = L"Обробка одновимірного масиву";
+			this->label1->Text = L"Введіть повне ім\'я";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(12, 170);
+			this->label4->Location = System::Drawing::Point(9, 117);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(141, 31);
 			this->label4->TabIndex = 6;
@@ -155,24 +135,32 @@ namespace Lab1 {
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(12, 214);
+			this->textBox2->Location = System::Drawing::Point(12, 168);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->ReadOnly = true;
-			this->textBox2->Size = System::Drawing::Size(479, 234);
+			this->textBox2->Size = System::Drawing::Size(479, 122);
 			this->textBox2->TabIndex = 7;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(12, 59);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(479, 38);
+			this->textBox1->TabIndex = 1;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(544, 555);
+			this->ClientSize = System::Drawing::Size(508, 409);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
@@ -185,63 +173,27 @@ namespace Lab1 {
 
 		}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		//перевірка чи введено масив
-		if (textBox1->Text == "") {
-			MessageBox::Show("Введіть масив через пробіл", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-			textBox1->Focus();
-			return;
+		//отримуємо введене ім'я
+		String^ full_name = textBox1->Text;
+
+		//знайдемо довжину імені
+		int length = full_name->Length;
+
+		//зробимо з імені масив через пробіл
+		array<String^>^ words = full_name->Split(' ');
+
+		//знайдемо ініціали для імені та по-батькові
+		String^ initials = "";
+		for (int i = 1; i < words->Length; i++) {
+			initials += words[i][0] + ". ";
 		}
 
-		//розділюємо введений рядок на масив чисел
-		array<String^>^ arr = textBox1->Text->Split(' ');
+		//виведемо результат
+		textBox2->Text = "Довжина імені: " + Convert::ToString(length) + "\r\n";
+		textBox2->Text += "Коротке ім'я: " + words[0] + " " + initials + "\r\n";
 
-		//рахуємо середнє арифметичне додатних елементів масиву
-		double sum = 0;
-		int count = 0;
-		for (int i = 0; i < arr->Length; i++) {
-			double num;
-			if (Double::TryParse(arr[i], num)) {
-				if (num > 0) {
-					sum += num;
-					count++;
-				}
-			}
-		}
-
-		double result = sum / count;
-
-		//сортування масиву методом екстремальних значень
-		for (int i = 0; i < arr->Length / 2; i++) {
-			double min = Double::Parse(arr[i]);
-			double max = Double::Parse(arr[i]);
-			int minIndex = i;
-			int maxIndex = i;
-			for (int j = i; j < arr->Length - i; j++) {
-				double num = Double::Parse(arr[j]);
-				if (num < min) {
-					min = num;
-					minIndex = j;
-				}
-				if (num > max) {
-					max = num;
-					maxIndex = j;
-				}
-			}
-			String^ temp = arr[i];
-			arr[i] = arr[minIndex];
-			arr[minIndex] = temp;
-
-			temp = arr[arr->Length - i - 1];
-			arr[arr->Length - i - 1] = arr[maxIndex];
-			arr[maxIndex] = temp;
-		}
-
-		//виведення результату
-		textBox2->Text = "Середнє арифметичне додатних елементів масиву: " + result + "\r\n";
-		textBox2->Text += "Масив після сортування: ";
-		for (int i = 0; i < arr->Length; i++) {
-			textBox2->Text += arr[i] + " ";
-		}
+		//переміщення фокусу
+		button2->Focus();
 
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
