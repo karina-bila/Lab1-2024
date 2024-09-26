9bf535936f8ff624eddc4ebfc265de672a006830
karina <karinaslusar246@gmail.com>
2024-09-26 22:56:32 +0300

    lab1-2024

--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -45,13 +45,20 @@ namespace Lab1 {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox3;
+
+
+
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -60,7 +67,10 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -69,8 +79,11 @@ namespace Lab1 {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -78,19 +91,19 @@ namespace Lab1 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(96, 157);
+			this->label1->Location = System::Drawing::Point(42, 136);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(51, 31);
+			this->label1->Size = System::Drawing::Size(49, 31);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"x =";
+			this->label1->Text = L"від";
 			// 
 			// textBox1
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(153, 154);
+			this->textBox1->Location = System::Drawing::Point(102, 133);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(299, 38);
+			this->textBox1->Size = System::Drawing::Size(48, 38);
 			this->textBox1->TabIndex = 1;
 			// 
 			// label2
@@ -98,21 +111,21 @@ namespace Lab1 {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(44, 9);
+			this->label2->Location = System::Drawing::Point(54, 9);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(493, 39);
+			this->label2->Size = System::Drawing::Size(356, 39);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"Обчислення значення функції";
+			this->label2->Text = L"Табулювання функції";
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(75, 314);
+			this->button1->Location = System::Drawing::Point(51, 314);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(127, 39);
+			this->button1->Size = System::Drawing::Size(153, 39);
 			this->button1->TabIndex = 3;
-			this->button1->Text = L"Обчислити";
+			this->button1->Text = L"Побудувати";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
@@ -120,7 +133,7 @@ namespace Lab1 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(245, 314);
+			this->button2->Location = System::Drawing::Point(210, 314);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(127, 39);
 			this->button2->TabIndex = 4;
@@ -144,38 +157,83 @@ namespace Lab1 {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(96, 221);
+			this->label4->Location = System::Drawing::Point(45, 206);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(51, 31);
 			this->label4->TabIndex = 6;
-			this->label4->Text = L"y =";
+			this->label4->Text = L"k =";
 			// 
 			// textBox2
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(153, 221);
+			this->textBox2->Location = System::Drawing::Point(99, 199);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->ReadOnly = true;
-			this->textBox2->Size = System::Drawing::Size(299, 38);
+			this->textBox2->Size = System::Drawing::Size(51, 38);
 			this->textBox2->TabIndex = 7;
 			// 
-			// pictureBox1
+			// chart1
 			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(138, 51);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(314, 83);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
-			this->pictureBox1->TabIndex = 8;
-			this->pictureBox1->TabStop = false;
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(526, 38);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Point;
+			series1->Legend = L"Legend1";
+			series1->Name = L"y(x)";
+			series2->ChartArea = L"ChartArea1";
+			series2->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series2->Legend = L"Legend1";
+			series2->Name = L"y(x1)";
+			this->chart1->Series->Add(series1);
+			this->chart1->Series->Add(series2);
+			this->chart1->Size = System::Drawing::Size(476, 377);
+			this->chart1->TabIndex = 9;
+			this->chart1->Text = L"chart1";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(69, 80);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(212, 31);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"Введіть відрізок";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(157, 136);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(42, 31);
+			this->label6->TabIndex = 11;
+			this->label6->Text = L"до";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox3->Location = System::Drawing::Point(208, 133);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(48, 38);
+			this->textBox3->TabIndex = 12;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(630, 468);
-			this->Controls->Add(this->pictureBox1);
+			this->ClientSize = System::Drawing::Size(1033, 468);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->chart1);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
@@ -186,45 +244,87 @@ namespace Lab1 {
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
-			this->Text = L"Function";
+			this->Text = L"Табулювання функції";
 			this->TopMost = true;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	//check if the input is correct and is double
-	if (textBox1->Text == "") {
-		MessageBox::Show("Введіть значення x", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		//check if the input is correct and is double
+		if (textBox1->Text == "") {
+			MessageBox::Show("Введіть значення a", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox1->Focus();
+			return;
+		}
+		if (textBox3->Text == "") {
+			MessageBox::Show("Введіть значення b", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox1->Focus();
+			return;
+		}
+		if (textBox2->Text == "") {
+			MessageBox::Show("Введіть значення k", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox1->Focus();
+			return;
+		}
+		double doubleValue;
+		if (!System::Double::TryParse(textBox1->Text, doubleValue)) {
+			MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox1->Focus();
+			return;
+		}
+		if (!System::Double::TryParse(textBox3->Text, doubleValue)) {
+			MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox1->Focus();
+			return;
+		}
+		if (!System::Double::TryParse(textBox2->Text, doubleValue)) {
+			MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox1->Focus();
+			return;
+		}
+
+
+		double a = Convert::ToDouble(textBox1->Text);
+		double b = Convert::ToDouble(textBox3->Text);
+		double k = Convert::ToDouble(textBox2->Text);
+
+		if (k <= 0) {
+			MessageBox::Show("k має бути більше 0", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			textBox2->Focus();
+			return;
+		}
+
+		double h = (b - a) / k;
+
+		chart1->Series["y(x)"]->Points->Clear();
+		chart1->Series["y(x1)"]->Points->Clear();
+
+		for (double i = a; i <= b; i = i + h) {
+			double x = a + i * h;
+			//y= ln(x^2+x+1)
+			double y = log(x * x + x + 1);
+			chart1->Series["y(x)"]->Points->AddXY(x, y);
+			chart1->Series["y(x1)"]->Points->AddXY(x, y);
+		}
 	}
-	double doubleValue;
-	if (!System::Double::TryParse(textBox1->Text, doubleValue)) {
-		MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		chart1->Series["y(x)"]->Points->Clear();
+		textBox1->Clear();
+		textBox2->Clear();
+		textBox3->Clear();
+
 		textBox1->Focus();
-		return;
 	}
-
-
-	double x = Convert::ToDouble(textBox1->Text);
-	double y = 0;
-		//y=((ln x^2 + cos x^2) / (cos2x +x^2)) + pow(x, 1/3)/x
-	y = ((log(pow(x, 2)) + cos(pow(x, 2))) / (cos(2 * x) + pow(x, 2))) + pow(x, 1.0 / 3) / x;
-	textBox2->Text = Convert::ToString(y);
-
-}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	textBox1->Clear();
-	textBox2->Clear();
-	textBox1->Focus();
-}
-private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	//center the form
-	this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
-		(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
-}
-};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		//center the form
+		this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
+			(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		chart1->Series["y(x)"]->Points->Clear();
+	}
+	};
 }
