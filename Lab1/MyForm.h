adceb19bcc5fc591abfe49fc48046ca21ac85c7d
karina <karinaslusar246@gmail.com>
2024-11-16 23:24:08 +0200

    Лабораторна 6

--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -1,5 +1,8 @@
 #pragma once
-#include <cmath>
+#include <cmath>]
+#include <vector>
+#include <iostream>
+#include <string>
 
 
 namespace Lab1 {
@@ -10,6 +13,7 @@ namespace Lab1 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -36,22 +40,29 @@ namespace Lab1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+
 	protected:
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
+
+
+
+
 	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::Button^ button2;
+
+
+
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -60,171 +71,204 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->tabPage2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(96, 157);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(51, 31);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"x =";
-			// 
-			// textBox1
-			// 
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(153, 154);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(299, 38);
-			this->textBox1->TabIndex = 1;
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(44, 9);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(493, 39);
-			this->label2->TabIndex = 2;
-			this->label2->Text = L"Обчислення значення функції";
-			// 
-			// button1
-			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(75, 314);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(127, 39);
-			this->button1->TabIndex = 3;
-			this->button1->Text = L"Обчислити";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// button2
-			// 
-			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(245, 314);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(127, 39);
-			this->button2->TabIndex = 4;
-			this->button2->Text = L"Очистити";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(12, 442);
+			this->label3->Location = System::Drawing::Point(13, 469);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(138, 17);
 			this->label3->TabIndex = 5;
 			this->label3->Text = L"© 2024, Біла Каріна";
 			// 
-			// label4
+			// tabControl1
 			// 
-			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(96, 221);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(51, 31);
-			this->label4->TabIndex = 6;
-			this->label4->Text = L"y =";
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Location = System::Drawing::Point(12, 1);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(606, 465);
+			this->tabControl1->TabIndex = 6;
 			// 
-			// textBox2
+			// tabPage1
 			// 
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(153, 221);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->ReadOnly = true;
-			this->textBox2->Size = System::Drawing::Size(299, 38);
-			this->textBox2->TabIndex = 7;
+			this->tabPage1->Controls->Add(this->dataGridView1);
+			this->tabPage1->Controls->Add(this->button1);
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(598, 439);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"Завдання 1";
+			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
-			// pictureBox1
+			// dataGridView1
 			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(138, 51);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(314, 83);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
-			this->pictureBox1->TabIndex = 8;
-			this->pictureBox1->TabStop = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(23, 7);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(545, 357);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(246, 384);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(88, 37);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Обчислити";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Controls->Add(this->dataGridView2);
+			this->tabPage2->Controls->Add(this->button2);
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(598, 439);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"Завдання 2";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Location = System::Drawing::Point(27, 12);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->Size = System::Drawing::Size(545, 357);
+			this->dataGridView2->TabIndex = 3;
+			this->dataGridView2->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView2_CellContentClick);
+			this->dataGridView2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(250, 389);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(88, 37);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Обчислити";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(630, 468);
-			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->label4);
+			this->ClientSize = System::Drawing::Size(630, 500);
+			this->Controls->Add(this->tabControl1);
 			this->Controls->Add(this->label3);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
 			this->Text = L"Function";
 			this->TopMost = true;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->tabPage2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	//check if the input is correct and is double
-	if (textBox1->Text == "") {
-		MessageBox::Show("Введіть значення x", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		//створюємо одновимірний масив з вказівником та заповнюємо його випадковими числами від -100 до 100
+		int size = 10;
+		int* arr = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			arr[i] = rand() % 201 - 100;
+		}
+
+		//заповнюємо таблицю даними
+		dataGridView1->ColumnCount = 1;
+		dataGridView1->RowCount = size;
+		dataGridView1->Columns[0]->HeaderText = "Масив";
+		for (int i = 0; i < size; i++) {
+			dataGridView1->Rows[i]->Cells[0]->Value = arr[i];
+		}
+
+		//знаходимо найменший елемент масиву та виводимо його на екран як messagebox
+		int min = arr[0];
+		for (int i = 1; i < size; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		MessageBox::Show("Мінімальний елемент масиву: " + min, "Результат");
+
+
 	}
-	double doubleValue;
-	if (!System::Double::TryParse(textBox1->Text, doubleValue)) {
-		MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		//створити двовимірний масив з vector
+		int rows = 10;
+		int cols = 10;
+		vector<vector<int>> arr(rows, vector<int>(cols));
+
+		//заповнити масив випадковими числами від -100 до 100
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				arr[i][j] = rand() % 201 - 100;
+			}
+		}
+
+		//заповнити таблицю даними
+		dataGridView2->ColumnCount = cols;
+		dataGridView2->RowCount = rows;
+
+		//робимо ширину стовпців однаковою
+		for (int i = 0; i < cols; i++) {
+			dataGridView2->Columns[i]->Width = 45;
+		}
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				dataGridView2->Rows[i]->Cells[j]->Value = arr[i][j];
+			}
+		}
+
+		//підрахувати середнє арифметичне від'ємних елементів масиву
+		double sum = 0;
+		int count = 0;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (arr[i][j] < 0) {
+					sum += arr[i][j];
+					count++;
+				}
+			}
+		}
+		double avg = sum / count;
+
+		MessageBox::Show("Середнє арифметичне від'ємних елементів масиву: " + avg, "Результат");
+
+
 	}
-
-
-	double x = Convert::ToDouble(textBox1->Text);
-	double y = 0;
-		//y=((ln x^2 + cos x^2) / (cos2x +x^2)) + pow(x, 1/3)/x
-	y = ((log(pow(x, 2)) + cos(pow(x, 2))) / (cos(2 * x) + pow(x, 2))) + pow(x, 1.0 / 3) / x;
-	textBox2->Text = Convert::ToString(y);
-
-}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	textBox1->Clear();
-	textBox2->Clear();
-	textBox1->Focus();
-}
-private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	//center the form
-	this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
-		(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
-}
-};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		//center the form
+		this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
+			(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
+	}
+	private: System::Void dataGridView2_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+	};
 }
