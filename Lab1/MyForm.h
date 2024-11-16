d87e47b805074d2f39ce019cd161ec13a03b4477
karina <karinaslusar246@gmail.com>
2024-11-16 23:45:38 +0200

    Лабораторна 8

--- Lab1/MyForm.h
+++ Lab1/MyForm.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cmath>
+#include <ctime>
 
 
 namespace Lab1 {
@@ -20,9 +21,8 @@ namespace Lab1 {
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			// initialaze global stack
+
 		}
 
 	protected:
@@ -43,15 +43,15 @@ namespace Lab1 {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Collections::Stack^ stack = gcnew System::Collections::Stack();
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -60,17 +60,14 @@ namespace Lab1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -78,37 +75,36 @@ namespace Lab1 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(96, 157);
+			this->label1->Location = System::Drawing::Point(42, 91);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(51, 31);
+			this->label1->Size = System::Drawing::Size(169, 31);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"x =";
+			this->label1->Text = L"Розмір стеку";
 			// 
 			// textBox1
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(153, 154);
+			this->textBox1->Location = System::Drawing::Point(227, 91);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(299, 38);
+			this->textBox1->Size = System::Drawing::Size(98, 38);
 			this->textBox1->TabIndex = 1;
 			// 
 			// label2
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
 			this->label2->Location = System::Drawing::Point(44, 9);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(493, 39);
+			this->label2->Size = System::Drawing::Size(541, 65);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"Обчислення значення функції";
+			this->label2->Text = L"Заповнити стек числами. Обчислити добуток непарних елементів стеку";
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(75, 314);
+			this->button1->Location = System::Drawing::Point(313, 306);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(127, 39);
 			this->button1->TabIndex = 3;
@@ -120,7 +116,7 @@ namespace Lab1 {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(245, 314);
+			this->button2->Location = System::Drawing::Point(313, 261);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(127, 39);
 			this->button2->TabIndex = 4;
@@ -139,45 +135,36 @@ namespace Lab1 {
 			this->label3->TabIndex = 5;
 			this->label3->Text = L"© 2024, Біла Каріна";
 			// 
-			// label4
+			// button3
 			// 
-			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(96, 221);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(51, 31);
-			this->label4->TabIndex = 6;
-			this->label4->Text = L"y =";
+			this->button3->Location = System::Drawing::Point(313, 185);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(127, 70);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"Додати до стеку";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
-			// textBox2
+			// listBox1
 			// 
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->listBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox2->Location = System::Drawing::Point(153, 221);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->ReadOnly = true;
-			this->textBox2->Size = System::Drawing::Size(299, 38);
-			this->textBox2->TabIndex = 7;
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(138, 51);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(314, 83);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
-			this->pictureBox1->TabIndex = 8;
-			this->pictureBox1->TabStop = false;
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 20;
+			this->listBox1->Location = System::Drawing::Point(50, 125);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(171, 284);
+			this->listBox1->TabIndex = 7;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(630, 468);
-			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->label4);
+			this->ClientSize = System::Drawing::Size(545, 468);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
@@ -186,45 +173,63 @@ namespace Lab1 {
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Function";
 			this->TopMost = true;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	//check if the input is correct and is double
-	if (textBox1->Text == "") {
-		MessageBox::Show("Введіть значення x", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		//якщо стек пустий вивести повідомлення
+		if (stack->Count == 0) {
+			MessageBox::Show("Стек пустий");
+			return;
+		}
+
+		//обчислити добуток непарних елементів стеку
+		int result = 1;
+		for each (int i in stack) {
+			if (i % 2 != 0) {
+				result *= i;
+			}
+		}
+
+		//вивести результат на екран
+		if (result == 1) {
+			MessageBox::Show("Немає непарних елементів");
+		}
+		else {
+			MessageBox::Show("Добуток непарних елементів стеку: " + result);
+		}
+
 	}
-	double doubleValue;
-	if (!System::Double::TryParse(textBox1->Text, doubleValue)) {
-		MessageBox::Show("Введіть число", "Помилка", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		textBox1->Focus();
-		return;
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		stack->Clear();
+		listBox1->Items->Clear();
 	}
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		srand(time(NULL));
+	};
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		int count = stack->Count;
+		int size = textBox1->Text != "" ? Convert::ToInt32(textBox1->Text) : 0;
 
 
-	double x = Convert::ToDouble(textBox1->Text);
-	double y = 0;
-		//y=((ln x^2 + cos x^2) / (cos2x +x^2)) + pow(x, 1/3)/x
-	y = ((log(pow(x, 2)) + cos(pow(x, 2))) / (cos(2 * x) + pow(x, 2))) + pow(x, 1.0 / 3) / x;
-	textBox2->Text = Convert::ToString(y);
+		if (count < size) {
+			stack->Push(rand() % 100);
+			//clear listbox
+			listBox1->Items->Clear();
+			for each (int i in stack) {
+				listBox1->Items->Add(i);
+			}
+		}
+		else {
+			MessageBox::Show("Стек повний");
+		}
 
-}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	textBox1->Clear();
-	textBox2->Clear();
-	textBox1->Focus();
-}
-private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	//center the form
-	this->Location = System::Drawing::Point((Screen::PrimaryScreen->Bounds.Width - this->Width) / 2,
-		(Screen::PrimaryScreen->Bounds.Height - this->Height) / 2);
-}
-};
+	}
+	};
 }
